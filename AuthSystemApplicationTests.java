2a4017bbba741374860c5437f7c6cd7f825b3f30
shrutim11s <shrutiimeshram@gmail.com>
2024-12-18 22:03:23 +0530

    Add files via upload

---
+++ AuthSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AuthSystemRBAC.AuthSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
